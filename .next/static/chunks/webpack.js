934b2754c1b1a08c76529b637b70596c810fc823
Cezary-Janicki <94046566+Cezary-Janicki@users.noreply.github.com>
2022-01-26 13:28:03 +0100

    misc changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fe180fb4cccc39d6"; }
+/******/ 		__webpack_require__.h = function() { return "b72482051075fbb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.285db396fb4a531f.hot-update.js
+++ .next/static/webpack/webpack.285db396fb4a531f.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "bc2d44e5ef781f2f"; }
+/******/ 	__webpack_require__.h = function() { return "b72482051075fbb0"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
